6658bd59b94e51e690c6923559552cebe5af7b8f
VictorPoiani <153679715+VictorPoiani@users.noreply.github.com>
2026-08-09 21:15:02 -0300

    Delete postit directory

--- postit/js/supabase.js
+++
@@ -1,7 +0,0 @@
-// js/supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabaseUrl = 'https://cqhicorrkhdcjyifsrin.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxaGljb3Jya2hkY2p5aWZzcmluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzNDY5OTUsImV4cCI6MjEwMDkyMjk5NX0.MQsH-jDeDDhQgrJWo1KIBJ9UIsHxVliFzI5yNUPW5u8';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
